1997aff49106f375b6da83dbe611c7c0c17a199f
crs_ctz <crs_ctz@yahoo.com>
2014-07-01 10:30:32 +0300

    done

---
+++ j1.js
@@ -0,0 +1,39 @@
+function cumparaturi($scope) {
+
+    $scope.todos = [];
+
+    $scope.controller = function () {
+        $scope.checked = !$scope.checked;
+    }
+
+    $scope.produs = {
+        text: '',
+        descriere: ''
+    };
+
+    $scope.addTodo = function() {
+
+            $scope.todos.push($scope.produs);
+            $scope.produs={
+                text: '',
+                descriere: ''};
+         }
+    };
+
+
+    $scope.archive = function() {
+        var oldTodos = $scope.todos;
+        $scope.todos = [];
+        angular.forEach(oldTodos, function(todo) {
+            if (!todo.done) $scope.todos.push(todo);
+        });
+
+
+    };
+
+
+
+
+
+
+
