c883644b3f7754045590b079216273b0449b476e
crs_ctz <crs_ctz@yahoo.com>
2014-07-01 11:47:18 +0300

    gata si editul

---
+++ j2.js
@@ -0,0 +1,20 @@
+function ClickToEditCtrl($scope) {
+
+    $scope.editorEnabled = false;
+
+    $scope.enableEditor = function() {
+        $scope.editorEnabled = true;
+        $scope.editableTitle = $scope.todo.text;
+        $scope.editableTitle1 = $scope.todo.descriere;
+    };
+
+    $scope.disableEditor = function() {
+        $scope.editorEnabled = false;
+    };
+
+    $scope.save = function() {
+        $scope.todo.text = $scope.editableTitle;
+        $scope.todo.descriere = $scope.editableTitle1;
+        $scope.disableEditor();
+    };
+}
